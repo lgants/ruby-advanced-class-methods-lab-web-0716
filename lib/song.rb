bce0267b37dcb126a704e393b4f2a2a75413df7f
lgants <lgants@gmail.com>
2016-07-29 18:31:33 -0400

    ruby advanced lab complete -ltg

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,8 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -10,4 +13,65 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect do |song_object|
+      song_object.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name)!=nil ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file)
+    song_details = file.split(" - ")
+    song = self.new
+    song.save
+    song.name = song_details[1].chomp(".mp3")
+    song.artist_name = song_details[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song_details = file.split(" - ")
+    song = self.new
+    song.name = song_details[1].chomp(".mp3")
+    song.artist_name = song_details[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
